4bee3d895968b6f00bd64c3d4600e9d10a7ec3a9
b2200765029 <92177067+b2200765029@users.noreply.github.com>
2021-11-05 16:16:56 +0300

    Update Exercise1.py

--- b2200765029_exercises/Exercise1.py
+++ b2200765029_exercises/Exercise1.py
@@ -10,7 +10,6 @@ for i in range(1, N + 1):
             evenNumbers = N / 2
             average = evenSum / evenNumbers
         else:
-            b
             evenNumbers= (N-1) / 2
             average = evenSum / evenNumbers
 print("The sum of odd numbers : {0}".format(oddSum))
